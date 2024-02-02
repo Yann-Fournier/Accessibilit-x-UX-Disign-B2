b03cbe03e4ef1101133b77a7f07d1d99a559765a
Yann-Fournier <y.fourni@icloud.com>
2024-02-02 12:48:12 +0100

    portfolio add experiences

--- Portfolio/js/main.js
+++ Portfolio/js/main.js
@@ -1,5 +1,17 @@
 var retour_haut = document.getElementById('retour-haut')
 
-retour_haut.addEventListener('click', function() {
+retour_haut.addEventListener('click', function () {
     window.scrollTo(0, 0);
 })
+
+var cards = document.querySelectorAll(".flip-card-inner");
+cards.forEach((card) => {
+    card.addEventListener('click', function () {
+        if (card.classList.contains('is-flipped')) {
+            card.classList.remove('is-flipped');
+        } else {
+            card.classList.add('is-flipped');
+        }
+
+    });
+});
